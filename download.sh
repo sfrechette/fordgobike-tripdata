7d7bce18d3f70ea294ca271468439c982a4d528e
Stephane Frechette <stephanefrechette@ukubu.com>
2019-03-08 12:41:14 -0500

    updates

--- download.sh
+++ download.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 FILE=$1
+S3BUCKET=s3://fordgobike-data
 
 cd data  
-wget https://s3.amazonaws.com/fordgobike-data/$1 
-echo "Done: $1"
-
+if [ -z "$1" ] 
+then
+    aws s3 cp ${S3BUCKET} . --recursive --exclude "*.html"
+    echo "Done"
+else
+    aws s3 cp "${S3BUCKET}/${1}" . 
+    echo "Done: $1"
+fi 
 
--- rename.sh
+++ rename.sh
@@ -6,7 +6,7 @@ for f in $(ls *.csv); do
     FILESUBSTR2=$(echo $f | cut -d'-' -f 2)
     FILESUBSTR3=$(echo $f | cut -d'-' -f 3 | cut -d'.' -f 1)
     FILESUBSTR4=$(echo $f | cut -d'.' -f 2) 
-    FILESTR="$FILESUBSTR2-$FILESUBSTR3-$FILESUBSTR1.$FILESUBSTR4"   
+    FILESTR="${FILESUBSTR2}-${FILESUBSTR3}-${FILESUBSTR1}.${FILESUBSTR4}"   
     mv $f $FILESTR
     echo "Done: $FILESTR"
 done
